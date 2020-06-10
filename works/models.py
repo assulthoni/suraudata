78cac4a815222145cc9d62adbe221050f383c4d4
Ahmad Shohibus Sulthoni <thoni.shohibus@gmail.com>
2020-06-10 21:57:31 +0700

    upload v2 with model

--- works/models.py
+++ works/models.py
@@ -8,6 +8,6 @@ class Posts(models.Model):
     category = models.CharField(max_length=4, choices=[('PRT','Portofolio'), ("IDE",'Idea')])
     created_at = models.DateField(default=datetime.date.today)
     content = models.TextField()
-
+    link = models.CharField(max_length=200, default="https://suraudata.xyz")
     def __str__(self):
         return self.title
