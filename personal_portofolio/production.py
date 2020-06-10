f59f56b86ef03d754f537a2e6e0a03b958f5db0b
Ahmad Shohibus Sulthoni <thoni.shohibus@gmail.com>
2020-06-10 22:19:06 +0700

    change settings conf static

--- personal_portofolio/production.py
+++ personal_portofolio/production.py
@@ -1,4 +1,13 @@
 # Production settings
 from settings.base import *
 DEBUG = False
-DATABASES = …
+DATABASES = {
+      'default': {
+    		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    		'NAME': 'suraudat_db_websuraudata',
+    		'USER': 'suraudat_jagoandata',
+    		'PASSWORD': 'qaua8XD3cR35pXJ' ,
+    		'HOST': 'localhost',
+    		'PORT': '5432',
+      }
+}
